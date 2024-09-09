35f73d958c64bd7bb8b27a9ac44904cebdfa8cd0
Eric Charnesky <EricCharnesky@gmail.com>
2024-09-09 14:12:51 -0400

    chaptetr 2

---
+++ Chapter2/main.py
@@ -0,0 +1,47 @@
+import math
+
+print(4 / 2)
+print("4 / 2")
+print(1/3)
+print(math.pi)
+
+first_number = 4
+second_number = 2
+
+my_favorite_number = 42
+
+print(first_number / second_number)
+
+first_number = 22
+
+second_number = first_number + my_favorite_number
+
+print(first_number / second_number)
+
+# camel casing
+firstNumber = 42
+
+# Please Excuse My Dear Aunt Sally
+print(9 / 3*(2+1))
+
+# a is for my first digit of the area code
+a = 1
+first_digit_of_area_code = 2
+c = 3
+
+first_digit_of_area_code = 6
+
+some_number_with_a_decimal = 4.2
+
+print(2 ** -3)
+
+
+name = input("Enter your name")
+
+print("Hello", name)
+print("hello name")
+
+# int( some_value_here ) - tries to convert the value to an integer
+age = int(input("enter your age"))
+
+print("You can retire in", 67 - age, "years")
