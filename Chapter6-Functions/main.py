40a0b63f92dff254cdf4069c7da9871bb95d4cd7
Eric Charnesky <EricCharnesky@gmail.com>
2024-10-09 13:15:50 -0400

    functions

--- Chapter6-Functions/main.py
+++ Chapter6-Functions/main.py
@@ -1,6 +1,110 @@
 import random
 
 
+
+def lucky_numbers_with_explict_tuple():
+    return (42, 7)
+
+def lucky_numbers():
+    return 42, 7 # automatically packs into a tuple
+
+
+def generate_citation(author, title, publisher="", year_published=2024):
+    print(f"{author}, {title}. {publisher}, {year_published}")
+
+
+
+# lists are mutable and can be changed in functions
+def upper_case_list(some_list):
+    for index in range(len(some_list)):
+        some_list[index] = some_list[index].upper()
+
+# strings are immutable and cannot be changed in functions
+def scream_in_internet(some_string):
+    some_string = some_string.upper()
+    return some_string
+
+# passing by value - not the location in memory
+def add_to(value):
+    value += 10
+
+def add_to_and_return(value):
+    value += 10
+    return value
+
+
+# automatic unpacking of a tuple returned
+lucky_number1, luck_number2 = lucky_numbers()
+
+# shortcut for doing it the long way
+tuple_lucky_numbers = lucky_numbers_with_explict_tuple()
+lucky_number3 = tuple_lucky_numbers[0]
+lucky_number4 = tuple_lucky_numbers[1]
+
+value = 12
+
+print(value)
+add_to(value)
+print(value)
+
+value = add_to_and_return(value)
+
+print(value)
+
+
+hello = "Happy Wednesday"
+scream_in_internet(hello)
+print(hello)
+
+
+drinks = ['Coffee', 'water', 'coke zero', 'matcha']
+
+print(drinks)
+upper_case_list(drinks)
+print(drinks)
+
+
+
+generate_citation("Eric", "some code", "myself", 2024)
+generate_citation(title="Some code", author="Eric Charnesky", year_published=2024, publisher="myself")
+generate_citation("eric", "some code", "myself")
+range(10, 20, 2)
+
+
+
+# try not to do this
+global some_number
+
+some_number = 20
+
+def wednesday():
+    some_number = 10 # local scope version
+    print("Print happy wednesday" * some_number)
+    print(some_number)
+
+print(some_number)
+
+print(wednesday())
+
+name = "Eric"
+
+print(len)
+
+print(len(name))
+
+#len = 10
+
+print(len)
+
+my_number = 42
+
+print(globals())
+
+
+
+wednesday()
+print(my_number)
+
 def finish_this_later(x, y, z):
     print("FIXME")
 
